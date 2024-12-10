5fb799d68d76a8fa18c6e7c8bbb0d8a5869868ac
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2024-12-10 16:59:46 -0700

    html elements hide/show to start game

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 console.log ("Hello World!")
 
 const startButton = document.getElementById('startButton');
-const tapButton = document.getElementById('startButton')
+const tapButton = document.getElementById('tapButton')
 const restartButton = document.getElementById('restartButton');
 const timer = document.getElementById('timer');
-const yourScore = document.getElementById('score');
+const yourScore = document.getElementById('yourScore');
+const instruction = document.getElementById('instructions');
 
 let score = 0;
 let timeLeft = 15;
+
+startButton.addEventListener('click', function(){
+    startButton.style.display = 'none';
+    tapButton.style.display = 'flex';
+    instruction.style.display = 'none';
+    startGame();
+});
 
