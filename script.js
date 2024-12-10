40d14dcd223c69aef9ca2f0130d9239fe90a96e6
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2024-12-10 16:45:53 -0700

    setting up HTML structure and a bit of JS code

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log ("Hello World!")
+
+const startButton = document.getElementById('startButton');
+const tapButton = document.getElementById('startButton')
+const restartButton = document.getElementById('restartButton');
+const timer = document.getElementById('timer');
+const yourScore = document.getElementById('score');
+
+let score = 0;
+let timeLeft = 15;
+
