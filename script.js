a598192b15844faa2f5d079dd335da84d1313737
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2024-12-11 14:07:02 -0700

    added updated record score displayed every time game ends

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ const timer = document.getElementById('timer');
 const yourScore = document.getElementById('yourScore');
 const instruction = document.getElementById('instructions');
 const hitsPerSecond = document.getElementById('hitsPerSecond');
+const recordScoreAndHits = document.getElementById('record');
 
 let score = 0;
 let timeLeft = 15;
 let intervalFunc;
 let hitPerSecondCalculated;
+let allScores = [];
+let allHitsPerSec = [];
+let allScoresCalculated;
+let allHitsPerSecCalculated;
 
 startButton.addEventListener('click', function(){
     startButton.style.display = 'none';
@@ -36,7 +41,6 @@ function startGame (){
         timeLeft--;
 
         timer.textContent = 'Time left: ' + timeLeft + ' seconds';
-
         if (timeLeft <= 0) {
             clearInterval(intervalFunc);
             endGame();
@@ -62,10 +66,16 @@ restartButton.addEventListener('click', function(){
 function endGame (){
     tapButton.style.display = 'none';
     restartButton.style.display = 'inline';
+    allScores.push(score);
     yourScore.textContent = 'Your score is: ' + score;
     yourScore.style.display = 'inline';
     timer.style.display = 'none';
     hitPerSecondCalculated = score / 15;
     hitsPerSecond.textContent = 'Hits per second: ' + hitPerSecondCalculated;
+    allHitsPerSec.push(hitPerSecondCalculated);
     hitsPerSecond.style.display = 'inline';
+    allScoresCalculated = Math.max(...allScores);
+    allHitsPerSecCalculated = Math.max(...allHitsPerSec);
+    recordScoreAndHits.textContent = 'Your record is: ' + allScoresCalculated + ' hits with ' + allHitsPerSecCalculated + ' hits/sec.'
+    recordScoreAndHits.style.display = 'inline';
 };
